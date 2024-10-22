9201a00b43b90291bae972e9f7955db54abb945d
Aran Bruce-Caddick <aranbruce@gmail.com>
2024-10-22 23:11:31 +0100

    updates for upgrade

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,9 @@
-export { GET, POST } from "@/app/(auth)/auth";
+import { GET as originalGET, POST as originalPOST } from "@/app/(auth)/auth";
+
+export const GET = (params: any) => {
+  return originalGET(params);
+};
+
+export const POST = (params: any) => {
+  return originalPOST(params);
+};

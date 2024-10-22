222a9f6b1afedcfbaf95ad9c45199d19f38f0e0c
Aran Bruce-Caddick <aranbruce@gmail.com>
2024-10-22 23:41:47 +0100

    package updates

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,1 @@
-import { GET as originalGET, POST as originalPOST } from "@/app/(auth)/auth";
-
-export const GET = (params: any) => {
-  return originalGET(params);
-};
-
-export const POST = (params: any) => {
-  return originalPOST(params);
-};
+export { GET, POST } from "@/app/(auth)/auth";

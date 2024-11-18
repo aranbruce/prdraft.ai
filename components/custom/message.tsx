ffb66254e8452c45baeb3024ef024a6d0dd1aebe
Aran <34005105+aranbruce@users.noreply.github.com>
2024-11-18 14:56:12 +0000

    Update message.tsx

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -38,7 +38,7 @@ export const PreviewMessage = ({
     >
       <div
         className={cx(
-          "flex w-full gap-4 rounded-xl group-data-[role=user]/message:ml-auto group-data-[role=user]/message:w-fit group-data-[role=user]/message:max-w-2xl group-data-[role=user]/message:bg-secondary group-data-[role=user]/message:px-3 group-data-[role=user]/message:py-2 group-data-[role=user]/message:text-secondary-foreground",
+          "flex w-full max-w-full gap-4 rounded-xl group-data-[role=user]/message:ml-auto group-data-[role=user]/message:w-fit group-data-[role=user]/message:max-w-2xl group-data-[role=user]/message:bg-secondary group-data-[role=user]/message:px-3 group-data-[role=user]/message:py-2 group-data-[role=user]/message:text-secondary-foreground",
         )}
       >
         {message.role === "assistant" && <Logo size={32} />}
